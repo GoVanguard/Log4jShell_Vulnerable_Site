533b28ed4680d5d82c10df8442958724d52962dc
S. Scott <sscott@govanguard.com>
2021-12-14 01:11:57 -0600

    Update log headers

--- src/main/java/com/example/log4j2demo/controller/Log4j2DemoController.java
+++ src/main/java/com/example/log4j2demo/controller/Log4j2DemoController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,8 +18,11 @@ public class Log4j2DemoController {
     private static final Logger logger = LogManager.getLogger(Log4j2DemoController.class);
 
 	@GetMapping("/hello")
-	public String getHello(Model model) {
+	//public String getHello(Model model) {
+    public String getHello(@RequestHeader("user-agent") String userAgent, Model model) {
 		model.addAttribute("hello","Hello Spring Boot User");
+        logger.debug("Logged the following user-agent:");
+        logger.debug(userAgent);
 		return "hello";
 	}
 
